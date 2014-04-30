5223820cbadad58b3b21d3ea283acfc6435bee7b
AndreRobbertse <AndreRobbertse@gmail.com>
2014-04-30 13:09:09 +0200

    commit

---
+++ GeoLocate/Controllers/RouteController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GeoLocate;
+
+namespace GeoLocate.Controllers
+{
+    public class RouteController : BaseController
+    {
+        private GeoLocateEntities db = new GeoLocateEntities();
+
+        // GET: /Route/
+        public async Task<ActionResult> Index()
+        {
+            var userroutes = db.UserRoutes.Include(u => u.AspNetUser);
+            return View(await userroutes.ToListAsync());
+        }
+
+        // GET: /Route/Details/5
+        public async Task<ActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserRoute userroute = await db.UserRoutes.FindAsync(id);
+            if (userroute == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userroute);
+        }
+
+        // GET: /Route/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "UserName");
+            return View();
+        }
+
+        // POST: /Route/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="ID,UserID,Name,Description,Timestamp")] UserRoute userroute)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserRoutes.Add(userroute);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "UserName", userroute.UserID);
+            return View(userroute);
+        }
+
+        // GET: /Route/Edit/5
+        public async Task<ActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserRoute userroute = await db.UserRoutes.FindAsync(id);
+            if (userroute == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "UserName", userroute.UserID);
+            return View(userroute);
+        }
+
+        // POST: /Route/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="ID,UserID,Name,Description,Timestamp")] UserRoute userroute)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userroute).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "UserName", userroute.UserID);
+            return View(userroute);
+        }
+
+        // GET: /Route/Delete/5
+        public async Task<ActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserRoute userroute = await db.UserRoutes.FindAsync(id);
+            if (userroute == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userroute);
+        }
+
+        // POST: /Route/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(long id)
+        {
+            UserRoute userroute = await db.UserRoutes.FindAsync(id);
+            db.UserRoutes.Remove(userroute);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
